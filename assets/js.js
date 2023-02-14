fe7297b391ebe77ffd373f5aac439adf988d79c8
Hong Man <hongman2807@gmail.com>
2023-02-14 22:57:50 +0700

    done!

---
+++ assets/js.js
@@ -0,0 +1,22 @@
+var a = [1,2,3,6,7,8,9]
+var b = [0]
+
+function outPut(a, b) {
+
+    let result = []
+    let obj = {}
+
+    for (let i=0; i<a.length; i++){
+        let num = a[i]
+        obj[num] = ""
+    }
+
+    for (let i=0; i<a.length; i++){
+        if (b[i] in obj) {
+            result.push(b[i])
+        }
+    }
+    return result
+}
+
+console.log(outPut(a,b))
